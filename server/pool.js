5495df41a1f87429ba8b7b36f857ffb2bf26f43d
dmoreir1 <dmoreir1@binghamton.edu>
2023-06-13 16:01:45 -0400

    starting on BE routes

--- server/pool.js
+++ server/pool.js
@@ -1,1 +1,14 @@
-console.log('This is my pool')
+const mysql = require('mysql2')
+
+exports.pool = mysql 
+    .createPool({
+
+        host: 'localhost',
+        user: 'root',
+        password: '123',
+        database: 'directory_dice'
+    })
+.promise()
+
+
+
