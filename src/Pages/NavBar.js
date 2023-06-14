7a83084e03e4987302b6d0b405ef91cb3822e862
SacoyaAdams <114244564+SacoyaAdams@users.noreply.github.com>
2023-06-14 11:04:30 -0400

    Update navbar

--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './HomePage.scss'
+import DDImage from "./DDLogo.png"
 
 export const NavBar = () => {
   return (
     <div className="NavBar">
       <button id="logo">
-        <img src='directorydice\src\Pages\DDLogo.png' alt='Logo'/>
+        <img src={DDImage} alt='Logo'/>
       </button>
       <button>About</button>
       <button>Contact</button>
