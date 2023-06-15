d82cb401b63eec594c3163203c1790edc29fd92c
Caridad RoblesCarvajal <croblescarvajal@xpanxion.com>
2023-06-15 11:52:59 -0400

    update profile photos

--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './NavBar.scss'
 import DDImage from "./DDLogo.png"
+
+
 import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <div className="NavBar">
-      <button className="navButton" id="logo"><Link to='/'><img src={DDImage} alt='Logo'/></Link></button>
+      <button className="navButton" id="logo"><Link to='/'><img src={ DDImage } alt='Logo'/></Link></button>
       <button className="navButton">Dashboard</button>
       <button className="navButton"><Link to='/about'>About</Link></button>
       <button className="navButton"><Link to='/contact'>Contact</Link></button>
