901b1fa8d8f9338f0e520d3c9697efabb5beff63
Dan Moreira <103558721+Dmoreir1@users.noreply.github.com>
2023-06-19 12:18:23 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import { HashRouter, Routes, Route } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+  
+
 root.render(
 
   <React.StrictMode>
-    <App />
+  <HashRouter>
+  <Routes>
+  <Route path = "/" element ={<App />}/>
+  </Routes>
+  </HashRouter>
   </React.StrictMode>
 
 );
